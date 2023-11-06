1b22be1d5972098a2939773a04e1743e0182af66
Amancayjesus136 <amancayjesus136@gmail.com>
2023-11-05 23:19:57 -0500

    No conecta a la BD

---
+++ src/Estudiante.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Estudiante extends JFrame{
+    private JPanel panel;
+    private JTextField idText;
+    private JTextField nombreTxt;
+    private JTextField apellidoText;
+    private JTextField telefonoTxt;
+    private JTextField carreraTxt;
+    private JButton ingresarBtn;
+    private JButton consultarBtn;
+    private JList lista;
+    private JTextField edadTxt;
+    Connection con;
+
+    public Estudiante() {
+        consultarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                conectar();
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        Estudiante f = new Estudiante();
+        f.setContentPane(new Estudiante().panel);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.pack();
+    }
+
+    public void conectar() {
+        try {
+            con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/learning", "root", "");
+            System.out.println("Conectado");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
